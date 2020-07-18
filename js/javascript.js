0a2c05af6a1029780e042ddf6200279286d82375
Angel Negron <negron.a@ufl.edu>
2020-07-18 01:56:40 -0400

    Add accordion javascript as well as corresponding html/css

--- js/javascript.js
+++ js/javascript.js
@@ -8,7 +8,31 @@ var visMenu = document.getElementsByTagName("ul")[0];
 //add event listener over button and set function to drop/hide meu
 ddMenu.addEventListener('click',
     function menuAppear() {
-        console.log("starting to work");
+        // console.log("starting to work");
         if (visMenu.style.display === "block") { visMenu.style.display = "none"; } else { visMenu.style.display = "block"; }
     }
 );
+
+//********* Accordions
+//variables that are or might be needed I think
+// var accordionArticle = document.getElementsByClassName("aboutarticle");
+// var accordionText = document.getElementsByClassName("aboutarticleText");
+var accordionHead = document.getElementsByClassName("h3about");
+
+for (i = 0; i < accordionHead.length; i++) {
+    accordionHead[i].addEventListener('click', function() {
+
+        console.log('This is kind of working');
+        this.classList.toggle("aboutarticletext");
+
+        var nextelement = this.nextElementSibling;
+        if (nextelement.style.display === "block") {
+            nextelement.style.display = "none";
+            // nextelement.style.transition = "2s";
+        } else {
+            nextelement.style.display = "block";
+
+        }
+
+    })
+}
